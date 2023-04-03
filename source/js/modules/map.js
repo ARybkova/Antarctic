8525ea264383f76bd59fd854a58f63a4e069a7f4
Alyona Rybkova <alyona.vorobyova.94@mail.ru>
2023-04-03 14:20:12 +0700

    Добавляет блоки с формой и контактами

---
+++ source/js/modules/map.js
@@ -0,0 +1,25 @@
+const COORDINATES = [59.938631, 30.323037];
+const ZOOM = 16;
+
+export const getMap = () => {
+  const yandexMap = window.ymaps;
+
+  yandexMap.ready(function () {
+    let myMap = new yandexMap.Map('map', {
+      center: COORDINATES,
+      zoom: ZOOM,
+    }, {
+      searchControlProvider: 'yandex#search',
+    });
+
+    let pin = new yandexMap.Placemark(COORDINATES, {}, {
+      iconLayout: 'default#image',
+      iconImageHref: './img/sprite/map-pin.svg',
+      iconImageSize: [18, 22],
+      iconImageOffset: [-9, -22],
+    });
+
+    myMap.geoObjects
+        .add(pin);
+  });
+};
